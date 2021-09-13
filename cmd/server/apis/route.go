9af00907b747f83d64cc1f47db28f8bcb952ff4b
tangx <uyinn@live.com>
2021-09-13 23:54:39 +0800

    feat: put file presign url

--- cmd/server/apis/route.go
+++ cmd/server/apis/route.go
@@ -14,6 +14,7 @@ func BaseRoute(base *gin.RouterGroup) {
 	objectRoute := v0Route.Group("/object")
 
 	objectRoute.GET("/*object", getHandler)
+	objectRoute.PUT("/*object", putHandler)
 }
 
 type Params struct {
@@ -36,4 +37,22 @@ func getHandler(c *gin.Context) {
 	}
 
 	c.String(http.StatusFound, u.String())
+}
+
+func putHandler(c *gin.Context) {
+	params := &Params{}
+
+	err := ginbinder.ShouldBindRequest(c, params)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bind params failed: %v", err)
+		return
+	}
+
+	u, err := global.S3.PreSignedPutURL(params.Object, false)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "internal error: %v", err)
+		return
+	}
+
+	c.String(http.StatusTemporaryRedirect, u.String())
 }
